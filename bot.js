a5a71d5aaeba6410f2043ea76a604c6295ff738f
Cryptoleonardo1 <cryptoleonardo1@gmail.com>
2024-08-20 01:07:19 +0200

    test 4

--- bot.js
+++ bot.js
@@ -2,8 +2,26 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, {polling: true});
 
+// Store user states
+const userStates = {};
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
+  sendWelcomeMessage(chatId);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  
+  // If it's not a /start command and we haven't sent a welcome message, send it
+  if (!msg.text.startsWith('/start') && !userStates[chatId]) {
+    sendWelcomeMessage(chatId);
+  }
+});
+
+function sendWelcomeMessage(chatId) {
+  userStates[chatId] = true; // Mark that we've sent the welcome message
+
   bot.sendMessage(chatId, "Welcome to the Pump Me Test Bot! Tap the button below to start the game.", {
     reply_markup: {
       inline_keyboard: [[
@@ -12,8 +30,9 @@ bot.onText(/\/start/, (msg) => {
           web_app: {url: 'https://pumpme-testbot.vercel.app'}
         }
       ]]
-    }
+    },
+    parse_mode: 'HTML'
   });
-});
+}
 
 console.log('Bot is running...');
