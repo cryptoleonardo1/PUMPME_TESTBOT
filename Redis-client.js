b081ece81b3d7ef6de5bf5e5e9feab3d4d4a72d3
Cryptoleonardo1 <cryptoleonardo1@gmail.com>
2024-09-04 00:33:36 +0200

    VPS test2

--- Redis-client.js
+++ Redis-client.js
@@ -2,6 +2,7 @@ const Redis = require('ioredis');
 
 const REDIS_URL = process.env.REDIS_URL;
 console.log('Redis URL exists:', !!REDIS_URL);
+console.log('Redis URL:', REDIS_URL ? REDIS_URL.replace(/\/\/.*@/, '//****@') : 'Not provided');
 
 if (!REDIS_URL) {
   throw new Error('REDIS_URL environment variable is not set');
@@ -16,9 +17,9 @@ const redis = new Redis(REDIS_URL, {
     return delay;
   },
   reconnectOnError(err) {
+    console.error('Redis reconnectOnError:', err);
     const targetError = 'READONLY';
     if (err.message.includes(targetError)) {
-      console.log('Reconnecting due to READONLY error');
       return true;
     }
     return false;
@@ -29,8 +30,16 @@ redis.on('connect', () => {
   console.log('Successfully connected to Redis');
 });
 
+redis.on('ready', () => {
+  console.log('Redis client is ready');
+});
+
 redis.on('error', (error) => {
   console.error('Redis connection error:', error);
+});
+
+redis.on('close', () => {
+  console.log('Redis connection closed');
 });
 
 module.exports = redis;
